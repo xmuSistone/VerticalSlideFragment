6648a24d24c9a937ea75054845efd7f4ec0122e1
zhangll <zhangll@lizi.com>
2016-04-05 16:53:09 +0800

    viewpager兼容

--- verticalSlide/src/com/stone/verticalslide/CustScrollView.java
+++ verticalSlide/src/com/stone/verticalslide/CustScrollView.java
@@ -7,8 +7,6 @@ import android.widget.ScrollView;
 
 public class CustScrollView extends ScrollView {
 	boolean allowDragBottom = true; // 如果是true，则允许拖动至底部的下一页
-	float downY = 0;
-	boolean needConsumeTouch = true; // 是否需要承包touch事件，needConsumeTouch一旦被定性，则不会更改
 
 	public CustScrollView(Context arg0) {
 		this(arg0, null);
@@ -22,39 +20,62 @@ public class CustScrollView extends ScrollView {
 		super(arg0, arg1, arg2);
 	}
 
+	private int scrollMode;
+	private static final int MODE_IDLE = 0;
+	private static final int MODE_HORIZONTAL = 1;
+	private static final int MODE_VERTICAL = 2;
+	private float downX, downY;
+	private boolean needConsumeTouch = false;
+	boolean isAtBottom = true; // 如果是true，则允许拖动至底部的下一页
+
 	@Override
 	public boolean dispatchTouchEvent(MotionEvent ev) {
 		if (ev.getAction() == MotionEvent.ACTION_DOWN) {
+			downX = ev.getRawX();
 			downY = ev.getRawY();
-			needConsumeTouch = true; // 默认情况下，scrollView内部的滚动优先，默认情况下由该ScrollView去消费touch事件
-
-			if (getScrollY() + getMeasuredHeight() >= computeVerticalScrollRange() - 2) {
-				// 允许向上拖动底部的下一页
-				allowDragBottom = true;
-			} else {
-				// 不允许向上拖动底部的下一页
-				allowDragBottom = false;
-			}
+			needConsumeTouch = true; // 默认情况下，listView内部的滚动优先，默认情况下由该listView去消费touch事件
+			isAtBottom = isAtBottom();
+			scrollMode = MODE_IDLE;
 		} else if (ev.getAction() == MotionEvent.ACTION_MOVE) {
-			if (!needConsumeTouch) {
+			if (!isAtBottom && scrollMode == MODE_VERTICAL) {
+				getParent().requestDisallowInterceptTouchEvent(true);
+				return super.dispatchTouchEvent(ev);
+			} else if (!needConsumeTouch || scrollMode == MODE_HORIZONTAL) {
 				// 在最顶端且向上拉了，则这个touch事件交给父类去处理
 				getParent().requestDisallowInterceptTouchEvent(false);
 				return false;
-			} else if (allowDragBottom) {
+			} else if (isAtBottom) {
 				// needConsumeTouch尚未被定性，此处给其定性
 				// 允许拖动到底部的下一页，而且又向上拖动了，就将touch事件交给父view
-				if (downY - ev.getRawY() > 2) {
+				if (ev.getRawY() - downY < 2) {
 					// flag设置，由父类去消费
 					needConsumeTouch = false;
 					getParent().requestDisallowInterceptTouchEvent(false);
-					return false;
+					return super.dispatchTouchEvent(ev);
+				}
+			}
+
+			if (scrollMode == MODE_IDLE) {
+				float xDistance = Math.abs(downX - ev.getRawX());
+				float yDistance = Math.abs(downY - ev.getRawY());
+				if (xDistance > yDistance && xDistance > 4) {
+					scrollMode = MODE_HORIZONTAL;
+				} else if (yDistance > xDistance && yDistance > 4) {
+					scrollMode = MODE_VERTICAL;
 				}
 			}
 		}
 
 		// 通知父view是否要处理touch事件
-		getParent().requestDisallowInterceptTouchEvent(needConsumeTouch);
+		getParent().requestDisallowInterceptTouchEvent(true);
 		return super.dispatchTouchEvent(ev);
+	}
+
+	private boolean isAtBottom() {
+		if (getScrollY() + getMeasuredHeight() >= computeVerticalScrollRange() - 2) {
+			return true;
+		}
+		return false;
 	}
 
 }
--- verticalSlide/src/com/stone/verticalslide/CustWebView.java
+++ verticalSlide/src/com/stone/verticalslide/CustWebView.java
@@ -7,8 +7,6 @@ import android.webkit.WebView;
 
 public class CustWebView extends WebView {
 	boolean allowDragTop = true; // 如果是true，则允许拖动至底部的下一页
-	float downY = 0;
-	boolean needConsumeTouch = true; // 是否需要承包touch事件，needConsumeTouch一旦被定性，则不会更改
 
 	public CustWebView(Context arg0) {
 		this(arg0, null);
@@ -22,43 +20,64 @@ public class CustWebView extends WebView {
 		super(arg0, arg1, arg2);
 	}
 
-	@Override
-	public boolean dispatchTouchEvent(MotionEvent ev) {
-		if (ev.getPointerCount() > 1) {
-			needConsumeTouch = true;
-		}
-		else if (ev.getAction() == MotionEvent.ACTION_DOWN) {
-			downY = ev.getRawY();
-			needConsumeTouch = true; // 默认情况下，listView内部的滚动优先，默认情况下由该listView去消费touch事件
-			allowDragTop = isAtTop();
-		} else if (ev.getAction() == MotionEvent.ACTION_MOVE) {
-			if (!needConsumeTouch) {
-				// 在最顶端且向上拉了，则这个touch事件交给父类去处理
-				getParent().requestDisallowInterceptTouchEvent(false);
-				return false;
-			} else if (allowDragTop) {
-				// needConsumeTouch尚未被定性，此处给其定性
-				// 允许拖动到底部的下一页，而且又向上拖动了，就将touch事件交给父view
-				if (ev.getRawY() - downY > 2) {
-					// flag设置，由父类去消费
-					needConsumeTouch = false;
-					getParent().requestDisallowInterceptTouchEvent(false);
-					return false;
-				}
-			}
-		}
+	private int scrollMode;
+    private static final int MODE_IDLE = 0;
+    private static final int MODE_HORIZONTAL = 1;
+    private static final int MODE_VERTICAL = 2;
+    private float downX, downY;
+    private boolean needConsumeTouch = false;
+    boolean isAtTop = true; // 如果是true，则允许拖动至底部的下一页
 
-		// 通知父view是否要处理touch事件
-		getParent().requestDisallowInterceptTouchEvent(needConsumeTouch);
-		return super.dispatchTouchEvent(ev);
-	}
+    @Override
+    public boolean dispatchTouchEvent(MotionEvent ev) {
+        if (ev.getAction() == MotionEvent.ACTION_DOWN) {
+            downX = ev.getRawX();
+            downY = ev.getRawY();
+            needConsumeTouch = true; // 默认情况下，listView内部的滚动优先，默认情况下由该listView去消费touch事件
+            isAtTop = isAtTop();
+            scrollMode = MODE_IDLE;
+        } else if (ev.getAction() == MotionEvent.ACTION_MOVE) {
+            if (!isAtTop && scrollMode == MODE_VERTICAL) {
+                getParent().requestDisallowInterceptTouchEvent(true);
+                return super.dispatchTouchEvent(ev);
+            }
+            else if (!needConsumeTouch || scrollMode == MODE_HORIZONTAL) {
+                // 在最顶端且向上拉了，则这个touch事件交给父类去处理
+                getParent().requestDisallowInterceptTouchEvent(false);
+                return false;
+            } else if (isAtTop) {
+                // needConsumeTouch尚未被定性，此处给其定性
+                // 允许拖动到底部的下一页，而且又向上拖动了，就将touch事件交给父view
+                if (ev.getRawY() - downY > 2) {
+                    // flag设置，由父类去消费
+                    needConsumeTouch = false;
+                    getParent().requestDisallowInterceptTouchEvent(false);
+                    return super.dispatchTouchEvent(ev);
+                }
+            }
 
-	/**
-	 * 判断listView是否在顶部
-	 * 
-	 * @return 是否在顶部
-	 */
-	private boolean isAtTop() {
-		return getScrollY() == 0;
-	}
+            if (scrollMode == MODE_IDLE) {
+                float xDistance = Math.abs(downX - ev.getRawX());
+                float yDistance = Math.abs(downY - ev.getRawY());
+                if (xDistance > yDistance && xDistance > 4) {
+                    scrollMode = MODE_HORIZONTAL;
+                } else if (yDistance > xDistance && yDistance > 4) {
+                    scrollMode = MODE_VERTICAL;
+                }
+            }
+        }
+
+        // 通知父view是否要处理touch事件
+        getParent().requestDisallowInterceptTouchEvent(true);
+        return super.dispatchTouchEvent(ev);
+    }
+
+    /**
+     * 判断listView是否在顶部
+     *
+     * @return 是否在顶部
+     */
+    private boolean isAtTop() {
+        return getScrollY() == 0;
+    }
 }
